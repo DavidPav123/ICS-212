67148d096d227b61e436611a306ecfc4c3d9892c
David P <davidpavlicek123@gmail.com>
2022-11-30 23:07:11 -1000

    Swapping Comp

--- Homework 10/main.cpp
+++ Homework 10/main.cpp
@@ -16,14 +16,20 @@
 //   Describe the file
 //
 //  REFERENCES:
-//   List references used here 
+//   List references used here
 //
 ****************************************************************/
 
+#include <vector>
+#include <map>
 #include "Bulbasaur.h"
 #include "Charmander.h"
 #include "Pokemon.h"
 #include "Squirtle.h"
+
+using namespace std;
+
+void checkPokedex(Pokemon* pokemon);
 
 /*****************************************************************
 //
@@ -44,7 +50,43 @@
 int main(int argc, char* argv[])
 {
     Pokemon* pokemon[3];
+    int i;
     pokemon[0] = new Squirtle();
     pokemon[1] = new Charmander();
     pokemon[2] = new Bulbasaur();
+
+    vector <string> nicknames = { "Squirt", "Charm", "Bulb" };
+
+    map <string, Pokemon*> Pokedex = {
+        {"Squirt", pokemon[0]},
+        {"Charm", pokemon[1]},
+        {"Bulb", pokemon[2]}
+    };
+
+    for (i = 0; i < nicknames.size(); i++)
+    {
+        checkPokedex(Pokedex[nicknames[i]]);
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        delete pokemon[i];
+    }
+}
+
+/*****************************************************************
+//
+//  Function name: checkPokedex
+//
+//  DESCRIPTION:   Prints the name of the given pokemon
+//
+//  Parameters:    pokemon (Pokemon*) : The pokemons name to check
+//
+//  Return values:  none
+//
+****************************************************************/
+
+void checkPokedex(Pokemon* pokemon)
+{
+    pokemon->printData();
 }
